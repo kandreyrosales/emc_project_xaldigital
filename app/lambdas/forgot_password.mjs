84ab79ddd06a15c54bafc93e589c6192f2ef9edd
Kevin <kandreyrosales@gmail.com>
2024-05-15 18:59:53 -0500

    Added Lambdas: Forgot Password and Confirm Forgot Password

---
+++ app/lambdas/forgot_password.mjs
@@ -0,0 +1,41 @@
+import { CognitoIdentityProviderClient, ForgotPasswordCommand } from "@aws-sdk/client-cognito-identity-provider";
+import crypto from 'crypto';
+
+const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
+
+function generateSecretHash(username, clientId, clientSecret) {
+    return crypto.createHmac('SHA256', clientSecret)
+                 .update(username + clientId)
+                 .digest('base64');
+}
+
+async function forgotPassword(event) {
+    const { username } = JSON.parse(event.body);
+    const clientId = process.env.clientId;
+    const clientSecret = process.env.clientSecret;
+
+    const secretHash = generateSecretHash(username, clientId, clientSecret);
+
+    const params = {
+        ClientId: clientId,
+        SecretHash: secretHash,
+        Username: username
+    };
+
+    try {
+        const command = new ForgotPasswordCommand(params);
+        const response = await client.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: response })
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message })
+        };
+    }
+}
+
+export { forgotPassword as handler };
