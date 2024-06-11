04ae0854c458c2ecd1557e531c85c1b4c165f8dc
kandreyrosales <kandreyrosales@gmail.com>
2024-06-11 15:21:00 -0500

    Added endpoint Blocks

--- app/app.py
+++ app/app.py
@@ -507,6 +507,6 @@ def exam_result():
         return jsonify({
             'id': exam_result_obj.id,
             'result_responses': exam_result_obj.respuestas,
-            'total_questions': len(json.loads(exam_result_obj.respuestas).get('questions'))
+            'total_questions': len(exam_result_obj.respuestas)
         })
     return jsonify({})
