6cec33b8bf85e6684cf9283b7d1d353eaceebfa5
kandreyrosales <kandreyrosales@gmail.com>
2024-06-10 22:27:04 -0500

    Added endpoint Blocks

--- app/app.py
+++ app/app.py
@@ -386,5 +386,5 @@ def get_question():
 
 @app.route('/send_exam_results', methods=['POST'])
 def send_exam_results():
-    result = request.json
+    result = request.json.get('exam_results')
     return jsonify({'message': [data for data in result]})
